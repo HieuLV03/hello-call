b86835ded6fd3fa5c1914fedb34a6438eda549e2
HieuLV03 <levanhieu14102k3@gmail.com>
2026-05-01 13:16:42 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let waitingUser = null;
+
+io.on("connection", (socket) => {
+  console.log("Connected:", socket.id);
+
+  if (waitingUser) {
+    const partner = waitingUser;
+
+    socket.partner = partner.id;
+    partner.partner = socket.id;
+
+    io.to(partner.id).emit("matched", socket.id);
+    io.to(socket.id).emit("matched", partner.id);
+
+    waitingUser = null;
+  } else {
+    waitingUser = socket;
+  }
+
+  socket.on("signal", ({ to, data }) => {
+    io.to(to).emit("signal", {
+      from: socket.id,
+      data,
+    });
+  });
+
+  socket.on("next", () => {
+    const partnerId = socket.partner;
+
+    if (partnerId) {
+      io.to(partnerId).emit("partner-disconnected");
+    }
+
+    socket.partner = null;
+
+    if (!waitingUser) {
+      waitingUser = socket;
+    }
+  });
+
+  socket.on("disconnect", () => {
+    if (waitingUser?.id === socket.id) {
+      waitingUser = null;
+    }
+
+    if (socket.partner) {
+      io.to(socket.partner).emit("partner-disconnected");
+    }
+  });
+});
+
+server.listen(3001, "0.0.0.0", () => {
+console.log("Server running on 3001");
+});
